00616b2bae1c876a2822c446a10528d5097b29d9
hive worker <zrashid_@hotmail.com>
2024-12-15 08:09:35 +0000

    1st commit

---
+++ src/main/java/com/heliosx_consultation/heliosx_consultation/HeliosxConsultationApplication.java
@@ -0,0 +1,13 @@
+package com.heliosx_consultation.heliosx_consultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeliosxConsultationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeliosxConsultationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heliosx_consultation/heliosx_consultation/HeliosxConsultationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heliosx_consultation.heliosx_consultation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeliosxConsultationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
